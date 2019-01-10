1d41845921d2f83aa3c386811d02be5c97de2cef
Cryomatrix <44329157+Cryomatrix@users.noreply.github.com>
2019-01-09 23:31:41 -0500

    Start of ctbalance side project

---
+++ counterbalance.py
@@ -0,0 +1,21 @@
+# Couterbalancing script for Franz #
+# Soraya Lahlou Jan 2019 #
+
+import numpy as np
+import pandas as pd
+
+''' import file '''
+file = 1
+
+''' restructure data '''
+
+''' some kind initialize var? '''
+initial = 0
+''' create loop '''
+while initial is 0:
+    # do some shuffling #
+    initial = 1
+
+
+
+
